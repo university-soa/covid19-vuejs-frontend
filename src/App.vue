fbc9aa7f8b069030c450e2b60e77ca08c40fe203
DenisDuev <denis_duev@abv.bg>
2020-05-24 13:38:59 +0300

    add banner at the begging of the page

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,11 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img alt="Vue logo" src="./assets/banner.png">
+    <div class="header">
+      <h1>Covid 19 News</h1>
+      <p>Sofia University SOA project</p>
+    </div>
+    <HelloWorld/>
     <CovidNews/>
   </div>
 </template>
